66060d7bb2a4fa3559e575ebc8cbe286b0e250cc
dustinxie <dustinxie@hotmail.com>
2019-07-15 17:31:57 -0700

    dropdown js

--- script.js
+++ script.js
@@ -11,4 +11,18 @@ $('.alert').on('closed.bs.alert',()=>{
 $('.carousel').carousel({
   interval: 1000
 })
+$('#dropdown-demo').on('show.bs.dropdown',()=>{
+  console.log('show')
+})
+$('#dropdown-demo').on('shown.bs.dropdown',()=>{
+  console.log('shown')
+})
+$('#dropdown-demo').on('hide.bs.dropdown',()=>{
+  console.log('hide')
+})
+$('#dropdown-demo').on('hidden.bs.dropdown',()=>{
+  console.log('hidden')
+})
+
+
 })
